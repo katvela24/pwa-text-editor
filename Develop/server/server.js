d457e42d463f57e7917a20e8ccc5c33287893944
katvela24 <156866399+katvela24@users.noreply.github.com>
2024-06-06 16:16:40 -0400

    edited src-sw.js file

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // app.use(express.static('../client/dist'));
-res.sendfile('./main.html');
+res.sendfile('./main.html');;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
